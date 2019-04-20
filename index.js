7fc5e7c482d4f59fd26d2852ea89271b6dfe4995
sirikwanYC <sirikwan_yanchan@hotmail.com>
2019-04-20 23:49:58 +0700

    no message

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 var express = require('express')
+const mongoose = require('mongoose')
 var app = express()
 
-app.set('port', (process.env.PORT || 5000))
+const urlDB = 'mongodb+srv://movie-ticket:aZA2pTp0PHHz1PZ2@movie-ticker-lq4km.gcp.mongodb.net/movie_ticket?retryWrites=true'
+
+mongoose.connect(urlDB, {useNewUrlParser: true}).then(
+    () => { console.log('connected') },
+    err => { console.log(err) }
+  )
+
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
   response.send('Hello World!')
 })
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
+app.listen(5000, function() {
+  console.log("Node app is running at localhost: 5000")
 })
