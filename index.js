708539f97223a9eedad3743266f0c6190804c60a
sirikwanYC <sirikwan_yanchan@hotmail.com>
2019-04-21 00:40:43 +0700

    no message

--- index.js
+++ index.js
@@ -29,8 +29,13 @@ app.get('/get-seat-movie/name-movie/:nameMovie/round-movie/:roundMovie', functio
 })
 
 app.post('/insert-ticket', function (request, response) {
-  var { movie_id, round_movie, movie_name, seat } = request.body
+  var movie_id = request.body.movie_id
+  var round_movie = request.body.round_movie
+  var movie_name = request.body.movie_name
+  var seat  = request.body.seat
+
   var newTicket = new Ticket(request.body)
+
   newTicket.save(function (err) {
     if (err) response.status(401).end()
     else response.status(200).end()
@@ -46,10 +51,12 @@ app.post('/insert-ticket', function (request, response) {
         newSeat.save(function () { })
       }
     })
+
 })
 
 app.get('/search-movie', function (request, response) {
-  var { name_movie, sort } = request.query
+  var name_movie = request.query.name_movie
+  var sort = request.query.sort
   Movie.find({
     $or: [
       { name_movie_en: { '$regex': name_movie, '$options': 'i' } },
