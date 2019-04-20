deedf4ac4a2d507b11e8b3a44e42d4d0141b93ee
sirikwanYC <sirikwan_yanchan@hotmail.com>
2019-04-21 00:13:21 +0700

    no message

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express')
 const mongoose = require('mongoose')
 var app = express()
+const { Movie } = require('movieModel.js')
 
 const urlDB = 'mongodb+srv://movie-ticket:aZA2pTp0PHHz1PZ2@movie-ticker-lq4km.gcp.mongodb.net/movie_ticket?retryWrites=true'
 
@@ -12,6 +13,11 @@ app.get('/', function (request, response) {
   response.send('Hello World!')
 })
 
+app.get('/get-all-movie', function (request, response) {
+  Movie.find({}, function (err, result) {
+    response.json(result)
+  }).lean()
+})
 
 app.listen(5000, function () {
   console.log("Node app is running at localhost: 5000")
