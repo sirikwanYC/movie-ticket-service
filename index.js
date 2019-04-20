5f26c0077a402a72c3f181baea0f5b8ccdfdc8ba
sirikwanYC <sirikwan_yanchan@hotmail.com>
2019-04-21 00:16:34 +0700

    no message

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 var express = require('express')
 const mongoose = require('mongoose')
 var app = express()
-const { Movie } = require('./movieModel.js')
 
 const urlDB = 'mongodb+srv://movie-ticket:aZA2pTp0PHHz1PZ2@movie-ticker-lq4km.gcp.mongodb.net/movie_ticket?retryWrites=true'
 
@@ -11,12 +10,6 @@ app.use(express.static(__dirname + '/public'))
 
 app.get('/', function (request, response) {
   response.send('Hello World!')
-})
-
-app.get('/get-all-movie', function (request, response) {
-  Movie.find({}, function (err, result) {
-    response.json(result)
-  }).lean()
 })
 
 app.listen(5000, function () {
